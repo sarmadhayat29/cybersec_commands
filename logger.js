2b317db56bfcf4ba307778258a8327f9ef5e0a89
Sarmad Hayat <152146751+sarmadhayat29@users.noreply.github.com>
2025-03-27 13:45:28 -0400

    logger.js || server.ts with changes

---
+++ logger.js
@@ -0,0 +1,11 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'security.log' })
+  ]
+});
+
+export default logger;
+
